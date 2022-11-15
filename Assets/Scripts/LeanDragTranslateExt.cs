e9fcd4bfed6bfe6c066d33bc7ad58e74f9feda21
Hugo Ferreira <hugodinis@ua.pt>
2022-11-15 17:55:01 +0000

    Multiplayer:Touch controls and physics now working
    For smoother physics maybe update with even smaller transform position
    thresholds

--- Assets/Scripts/LeanDragTranslateExt.cs
+++ Assets/Scripts/LeanDragTranslateExt.cs
@@ -2,35 +2,265 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Lean.Touch;
+using Unity.Netcode;
+using CW.Common;
+
+/// <summary>This component allows you to translate the current GameObject relative to the camera using the finger drag gesture.</summary>
+[HelpURL(LeanTouch.HelpUrlPrefix + "LeanDragTranslate")]
+[AddComponentMenu(LeanTouch.ComponentPathPrefix + "Drag Translate")]
+public class LeanDragTranslate : NetworkBehaviour
+{
+	/// <summary>The method used to find fingers to use with this component. See LeanFingerFilter documentation for more information.</summary>
+	public LeanFingerFilter Use = new LeanFingerFilter(true);
+
+	/// <summary>The camera the translation will be calculated using.
+	/// None/null = MainCamera.</summary>
+	public Camera Camera { set { _camera = value; } get { return _camera; } }
+	[SerializeField] private Camera _camera;
+
+	/// <summary>The movement speed will be multiplied by this.
+	/// -1 = Inverted Controls.</summary>
+	public float Sensitivity { set { sensitivity = value; } get { return sensitivity; } }
+	[SerializeField] private float sensitivity = 1.0f;
+
+	/// <summary>If you want this component to change smoothly over time, then this allows you to control how quick the changes reach their target value.
+	/// -1 = Instantly change.
+	/// 1 = Slowly change.
+	/// 10 = Quickly change.</summary>
+	public float Damping { set { damping = value; } get { return damping; } }
+	[SerializeField] private float damping = -1.0f;
+
+	/// <summary>This allows you to control how much momentum is retained when the dragging fingers are all released.
+	/// NOTE: This requires <b>Dampening</b> to be above 0.</summary>
+	public float Inertia { set { inertia = value; } get { return inertia; } }
+	[SerializeField] [Range(0.0f, 1.0f)] private float inertia;
+
+	[SerializeField]
+	private Vector3 remainingTranslation;
+
+	/// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually add a finger.</summary>
+	public void AddFinger(LeanFinger finger)
+	{
+		Use.AddFinger(finger);
+	}
+
+	/// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually remove a finger.</summary>
+	public void RemoveFinger(LeanFinger finger)
+	{
+		Use.RemoveFinger(finger);
+	}
+
+	/// <summary>If you've set Use to ManuallyAddedFingers, then you can call this method to manually remove all fingers.</summary>
+	public void RemoveAllFingers()
+	{
+		Use.RemoveAllFingers();
+	}
+
+#if UNITY_EDITOR
+	protected virtual void Reset()
+	{
+		Use.UpdateRequiredSelectable(gameObject);
+	}
+#endif
+
+	protected virtual void Awake()
+	{
+		Use.UpdateRequiredSelectable(gameObject);
+	}
+
+	protected virtual void Update()
+	{
+		// Store
+		var oldPosition = transform.localPosition;
+
+		// Get the fingers we want to use
+		var fingers = Use.UpdateAndGetFingers();
+
+		// Calculate the screenDelta value based on these fingers
+		var screenDelta = LeanGesture.GetScreenDelta(fingers);
+
+		if (screenDelta != Vector2.zero)
+		{
+			// Perform the translation
+			if (transform is RectTransform)
+			{
+				TranslateUI(screenDelta);
+			}
+			else
+			{
+				Translate(screenDelta);
+			}
+		}
+
+		// Increment
+		remainingTranslation += transform.localPosition - oldPosition;
+
+		// Get t value
+		var factor = CwHelper.DampenFactor(Damping, Time.deltaTime);
+
+		// Dampen remainingDelta
+		var newRemainingTranslation = Vector3.Lerp(remainingTranslation, Vector3.zero, factor);
+
+		// Shift this transform by the change in delta
+		transform.localPosition = oldPosition + remainingTranslation - newRemainingTranslation;
+
+		if (fingers.Count == 0 && Inertia > 0.0f && Damping > 0.0f)
+		{
+			newRemainingTranslation = Vector3.Lerp(newRemainingTranslation, remainingTranslation, Inertia);
+		}
+
+		// Update remainingDelta with the dampened value
+		remainingTranslation = newRemainingTranslation;
+	}
+
+	private void TranslateUI(Vector2 screenDelta)
+	{
+		var camera = this._camera;
+
+		if (camera == null)
+		{
+			var canvas = transform.GetComponentInParent<Canvas>();
+
+			if (canvas != null && canvas.renderMode != RenderMode.ScreenSpaceOverlay)
+			{
+				camera = canvas.worldCamera;
+			}
+		}
+
+		// Screen position of the transform
+		var screenPoint = RectTransformUtility.WorldToScreenPoint(camera, transform.position);
+
+		// Add the deltaPosition
+		screenPoint += screenDelta * Sensitivity;
+
+		// Convert back to world space
+		var worldPoint = default(Vector3);
+
+		if (RectTransformUtility.ScreenPointToWorldPointInRectangle(transform.parent as RectTransform, screenPoint, camera, out worldPoint) == true)
+		{
+			transform.position = worldPoint;
+		}
+	}
+
+	private void Translate(Vector2 screenDelta)
+	{
+		// Make sure the camera exists
+		var camera = CwHelper.GetCamera(this._camera, gameObject);
+
+		if (camera != null)
+		{
+			// Screen position of the transform
+			var screenPoint = camera.WorldToScreenPoint(transform.position);
+
+			// Add the deltaPosition
+			screenPoint += (Vector3)screenDelta * Sensitivity;
+
+			// Convert back to world space
+			transform.position = camera.ScreenToWorldPoint(screenPoint);
+		}
+		else
+		{
+			Debug.LogError("Failed to find camera. Either tag your camera as MainCamera, or set one in this component.", this);
+		}
+	}
+}
+
+#if UNITY_EDITOR
+namespace Lean.Touch.Editor
+{
+	using UnityEditor;
+	using TARGET = LeanDragTranslate;
+
+	[CanEditMultipleObjects]
+	[CustomEditor(typeof(TARGET), true)]
+	public class LeanDragTranslate_Editor : CwEditor
+	{
+		protected override void OnInspector()
+		{
+			TARGET tgt; TARGET[] tgts; GetTargets(out tgt, out tgts);
+
+			Draw("Use");
+			Draw("_camera", "The camera the translation will be calculated using.\n\nNone/null = MainCamera.");
+			Draw("sensitivity", "The movement speed will be multiplied by this.\n\n-1 = Inverted Controls.");
+			Draw("damping", "If you want this component to change smoothly over time, then this allows you to control how quick the changes reach their target value.\n\n-1 = Instantly change.\n\n1 = Slowly change.\n\n10 = Quickly change.");
+			Draw("inertia", "This allows you to control how much momentum is retained when the dragging fingers are all released.\n\nNOTE: This requires <b>Damping</b> to be above 0.");
+		}
+	}
+}
+#endif
+
 
 [RequireComponent(typeof(Rigidbody))]
 public class LeanDragTranslateExt : LeanDragTranslate
 {
 
-    Rigidbody rb;
-    bool isGrabbed = false;
+	Rigidbody rb;
+	bool isGrabbed = false;
+	bool isMoving = false;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	override protected void Update()
+	{
+		if (OnChangeImageTarget.isImageTargetOn & !isGrabbed)
+		{
+			var fingers = Use.UpdateAndGetFingers();
+			if (fingers.Count >= 1)
+			{
+				Debug.Log("Trying to grab!");
+				rb.velocity = Vector3.zero;
+				ChangeOwnership();
+				isMoving = true;
+			}
+			else if (isMoving) {
+				RemoveOwnership();
+			}
+			base.Update();
+
+			rb.useGravity = fingers.Count == 0;
+			//RemoveOwnershipServerRpc();
+		}
+		else {
+			// have to call UpdateAndGetFingers() every frame as noted in the same function notes
+			var fingers = Use.UpdateAndGetFingers();
+		}
+
+	}
+
+	public void ChangeOwnership() {
+		// If it already is the owner or another player is the owner don't do anything
+		if (IsOwner || !IsOwnedByServer) return;
+
+		StopAllCoroutines();
+		ChangeOwnershipServerRpc(NetworkManager.Singleton.LocalClientId);
+	}
+
+	public void RemoveOwnership()
+	{
+		// If it isn't the owner don't do anything
+		if (!IsOwner) return;
+
+		StartCoroutine(DelayRemoveOwnership());
+	}
+
+	IEnumerator DelayRemoveOwnership() {
+		yield return new WaitForSeconds(0.08f);
+		RemoveOwnershipServerRpc();
+	}
+
+	[ServerRpc(RequireOwnership = false)]
+    public void ChangeOwnershipServerRpc(ulong localClientId) {
+        GetComponent<NetworkObject>().ChangeOwnership(localClientId);
+		Debug.Log("changed ownership");
     }
-
-    override protected void Update()
+    [ServerRpc(RequireOwnership = false)]
+    public void RemoveOwnershipServerRpc()
     {
-        if (OnChangeImageTarget.isImageTargetOn & !isGrabbed)
-        {
-            base.Update();
-            var fingers = Use.UpdateAndGetFingers();
-            rb.useGravity = fingers.Count == 0;
-            if (fingers.Count >= 1)
-            {
-                rb.velocity = Vector3.zero;
-            }
-        }
-        else {
-            // have to call UpdateAndGetFingers() every frame as noted in the same function notes
-            var fingers = Use.UpdateAndGetFingers();
-        }
+		Debug.Log("removed ownership");
+		GetComponent<NetworkObject>().RemoveOwnership();
     }
 
     public void SetIsGrabbed(bool state) {
